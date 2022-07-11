b0cfda31350777f9eee4cc11d215de06929777b2
casksteven <casksteven@gmail.com>
2022-07-10 22:32:53 -0400

    test

--- src/components/online.js
+++ src/components/online.js
@@ -8,8 +8,10 @@ export default function App({ server = "sg" }) {
   var host;
   if(server == "天理一服 地址：电脑：1.casks.me 手机：https://1.casks.me"){
     host = "1.casks.me"
+    seraddress = "电脑：2.casks.me 手机：https://2.casks.me"
   } else if(server == "天理二服 电脑：2.casks.me 手机：https://2.casks.me"){
     host = "2.casks.me"
+    seraddress = "电脑：.casks.me 手机：https://.casks.me"
   } else if(server == "天理魂服 电脑：soul.casks.me 手机：https://soul.casks.me"){
     host = "soul.casks.me"
   } else if(server == "天理2.8服 电脑：28.casks.me 手机：https://28.casks.me"){
@@ -36,8 +38,9 @@ export default function App({ server = "sg" }) {
 
       <div class="stat">
         <div class="stat-title">{server}</div>
-        <div class="title">{online}</div>
+        <div class="stat-value">{online}</div>
         <div class="stat-desc">在线人数</div>
+        <div class="stat-desc">{seraddress}</div>
       </div>
 
   );
