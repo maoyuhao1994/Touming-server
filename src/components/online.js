edf622e1aa33029fe64f8a7df5e741bdbdb21796
casksteven <casksteven@gmail.com>
2022-07-10 22:17:40 -0400

    fix name error

--- src/components/online.js
+++ src/components/online.js
@@ -5,13 +5,13 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 export default function App({ server = "sg" }) {
 
   var host;
-  if(server == "天理一服"){
+  if(server == "天理一服 地址：电脑：1.casks.me 手机：https://1.casks.me"){
     host = "1.casks.me"
-  } else if(server == "天理二服"){
+  } else if(server == "天理二服 电脑：2.casks.me  手机：https://2.casks.me"){
     host = "2.casks.me"
-  } else if(server == "天理魂服"){
+  } else if(server == "天理魂服 电脑：soul.casks.me  手机：https://soul.casks.me"){
     host = "soul.casks.me"
-  } else if(server == "天理2.8服"){
+  } else if(server == "天理2.8服 电脑：28.casks.me  手机：https://28.casks.me"){
     host = "28.casks.me"
   }
   
