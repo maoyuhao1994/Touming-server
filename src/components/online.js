146219aeeef08f69182d89e8d7ee3b02c05a7323
casksteven <casksteven@gmail.com>
2022-07-10 22:39:44 -0400

    fix

--- src/components/online.js
+++ src/components/online.js
@@ -17,6 +17,17 @@ export default function App({ server = "sg" }) {
   } else if(server == "天理2.8服 电脑：28.casks.me 手机：https://28.casks.me"){
     host = "28.casks.me"
   }
+
+  var seraddress;
+  if(server == "天理一服 地址：电脑：1.casks.me 手机：https://1.casks.me"){
+    seraddress = "电脑：2.casks.me 手机：https://2.casks.me"
+  } else if(server == "天理二服 电脑：2.casks.me 手机：https://2.casks.me"){
+    seraddress = "电脑：.casks.me 手机：https://.casks.me"
+  } else if(server == "天理魂服 电脑：soul.casks.me 手机：https://soul.casks.me"){
+    seraddress = "电脑：.casks.me 手机：https://.casks.me"
+  } else if(server == "天理2.8服 电脑：28.casks.me 手机：https://28.casks.me"){
+    seraddress = "电脑：.casks.me 手机：https://.casks.me"
+  }
   
   const { data, error } = useSWR(
     `https://${host}/status/server`,
