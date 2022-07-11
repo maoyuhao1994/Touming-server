8cb32168866ec91105fcd2b98102aea38511043c
casksteven <casksteven@gmail.com>
2022-07-10 22:43:56 -0400

    fix

--- src/components/online.js
+++ src/components/online.js
@@ -6,29 +6,21 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 export default function App({ server = "sg" }) {
 
   var host;
-  if(server == "天理一服 地址：电脑：1.casks.me 手机：https://1.casks.me"){
+  var seraddress;
+  if(server == "天理一服"){
     host = "1.casks.me"
-    seraddress = "电脑：2.casks.me 手机：https://2.casks.me"
-  } else if(server == "天理二服 电脑：2.casks.me 手机：https://2.casks.me"){
+    seraddress = "电脑：1.casks.me 手机：https://1.casks.me"
+  } else if(server == "天理二服"){
     host = "2.casks.me"
-    seraddress = "电脑：.casks.me 手机：https://.casks.me"
-  } else if(server == "天理魂服 电脑：soul.casks.me 手机：https://soul.casks.me"){
+    seraddress = "电脑：2.casks.me 手机：https://2.casks.me"
+  } else if(server == "天理魂服"){
     host = "soul.casks.me"
-  } else if(server == "天理2.8服 电脑：28.casks.me 手机：https://28.casks.me"){
+    seraddress = "电脑：soul.casks.me 手机：https://soul.casks.me"
+  } else if(server == "天理2.8服"){
     host = "28.casks.me"
+    seraddress = "电脑：28.casks.me 手机：https://28.casks.me"
   }
 
-  var seraddress;
-  if(server == "天理一服 地址：电脑：1.casks.me 手机：https://1.casks.me"){
-    seraddress = "电脑：2.casks.me 手机：https://2.casks.me"
-  } else if(server == "天理二服 电脑：2.casks.me 手机：https://2.casks.me"){
-    seraddress = "电脑：.casks.me 手机：https://.casks.me"
-  } else if(server == "天理魂服 电脑：soul.casks.me 手机：https://soul.casks.me"){
-    seraddress = "电脑：.casks.me 手机：https://.casks.me"
-  } else if(server == "天理2.8服 电脑：28.casks.me 手机：https://28.casks.me"){
-    seraddress = "电脑：.casks.me 手机：https://.casks.me"
-  }
-  
   const { data, error } = useSWR(
     `https://${host}/status/server`,
     fetcher
--- src/pages/index.js
+++ src/pages/index.js
@@ -32,16 +32,16 @@ export default function Home({ allPostsData }) {
         <div class="text-center py-4">
           
           <div class="stats bg-primary text-primary-content servers">
-            <Online server="天理一服 地址：电脑：1.casks.me 手机：https://1.casks.me" />
-            <Online server="天理二服 电脑：2.casks.me 手机：https://2.casks.me" />
-            <Online server="天理魂服 电脑：soul.casks.me 手机：https://soul.casks.me" />
-            <Online server="天理2.8服 电脑：28.casks.me 手机：https://28.casks.me" />
+            <Online server="天理一服" />
+            <Online server="天理二服" />
+            <Online server="天理魂服" />
+            <Online server="天理2.8服" />
           </div>
 
         </div>
         
         <p class="text-center">
-          服务器地址是输入到代理里面的，请勿将其直接输入浏览器，不好的请看下方教程<br/>
+          服务器地址是输入到代理里面的，请勿将其直接输入浏览器，不会请看下方教程<br/>
           电脑的端口留空或者输443就好 <br/>
         </p>
 
